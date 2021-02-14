51853b8b5bb7209a123cea80fd03f4bd2c89d0a6
seaiam <65039814+seaiam@users.noreply.github.com>
2021-02-13 19:30:39 -0500

    #6
    Log-in using facebook. Basic html templates

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from pigeonpost import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),         path("login/", views.login, name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path('social-auth/', include('social_django.urls', namespace="social")),
+    path("", views.home, name="home"),
 ]
