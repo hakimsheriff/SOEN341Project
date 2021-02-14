50321fded7351a956cd8373108fe934e5e093708
seaiam <65039814+seaiam@users.noreply.github.com>
2021-02-13 19:42:56 -0500

    Revert "#6"
    This reverts commit 51853b8b5bb7209a123cea80fd03f4bd2c89d0a6.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,13 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from pigeonpost import views
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),         path("login/", views.login, name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path('social-auth/', include('social_django.urls', namespace="social")),
-    path("", views.home, name="home"),
+    path('admin/', admin.site.urls),
 ]
