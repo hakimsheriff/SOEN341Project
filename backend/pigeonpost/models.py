c2e25e06627e12268582fc532e24b5badb38768e
seaiam <65039814+seaiam@users.noreply.github.com>
2021-02-24 20:41:30 -0500

    #9 #1 Created feed page
    #9 #1
    - added /feed url to allow user to upload picture with caption
    - pip install pillow , pip install pusher

--- backend/pigeonpost/models.py
+++ backend/pigeonpost/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Feed(models.Model):
+        description = models.CharField(max_length=255, blank=True)
+        document = models.ImageField(upload_to='pigeonpost/static/images')
 
 
