cc5b9c2d67a6e1deb7958e5a4d48dc1052aa5800
seaiam <65039814+seaiam@users.noreply.github.com>
2021-02-25 21:27:05 -0500

    #9
    #9
    -Made alternative way to upload picture,
    - created picture feed and page for upload
    - issue with the path of pictures

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'pigeonpost'
+    'pigeonpost',
+    'crispy_forms', ## make sure to pip install django-crispy-forms
 ]
 
 MIDDLEWARE = [
--- backend/pigeonpost/views.py
+++ backend/pigeonpost/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.urls import reverse
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.contrib.auth.models import User
+from .forms import NewCommentForm, NewPostForm
+from django.views.generic import ListView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post, Comments, Like
+from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+import json
 # Create your views here.
 def login(request, user):
   return render(request, 'login.html')
@@ -27,3 +38,72 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
+
+class PostListView(ListView):
+  model = Post
+  template_name = 'home.html'
+  context_object_name = 'posts'
+  ordering = ['-date_posted']
+  paginate_by = 10
+  def get_context_data(self, **kwargs):
+		        context = super(PostListView, self).get_context_data(**kwargs)
+		        if self.request.user.is_authenticated:
+			              liked = [i for i in Post.objects.all() if Like.objects.filter(user = self.request.user, post=i)]
+			              context['liked_post'] = liked
+		        return context
+
+class UserPostListView(LoginRequiredMixin, ListView):	
+  model = Post
+  template_name = 'feed/user_posts.html'
+  context_object_name = 'posts'
+  paginate_by = 10
+
+  def get_context_data(self, **kwargs):		
+    context = super(UserPostListView, self).get_context_data(**kwargs)
+    user = get_object_or_404(User, username=self.kwargs.get('username'))
+    liked = [i for i in Post.objects.filter(user_name=user) if Like.objects.filter(user = self.request.user, post=i)]
+    context['liked_post'] = liked
+    return context	
+  
+  def get_queryset(self):
+    user = get_object_or_404(User, username=self.kwargs.get('username'))
+    return Post.objects.filter(user_name=user).order_by('-date_posted')
+
+@login_required
+def post_detail(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  user = request.user
+  is_liked =  Like.objects.filter(user=user, post=post)
+  if request.method == 'POST':
+        form = NewCommentForm(request.POST)
+        if form.is_valid():			
+          data = form.save(commit=False)
+          data.post = post
+          data.username = user
+          data.save()
+          return redirect('post-detail', pk=pk)
+  else:
+        form = NewCommentForm()
+  return render(request, 'feed/post_detail.html',{'post':post, 'is_liked':is_liked, 'form':form})
+
+@login_required
+def create_post(request): ## this is for posting a picture
+	user = request.user
+	if request.method == "POST":
+		form = NewPostForm(request.POST, request.FILES)
+		if form.is_valid():
+			data = form.save(commit=False)
+			data.user_name = user
+			data.save()
+			messages.success(request, f'Posted Successfully')
+			return redirect('home')
+	else:
+		form = NewPostForm()
+	return render(request, 'createPost.html', {'form':form})
+
+@login_required ## to delete a picture
+def post_delete(request, pk):
+	post = Post.objects.get(pk=pk)
+	if request.user== post.user_name:
+		Post.objects.get(pk=pk).delete()
+	return redirect('home') 
