1ddd58f35599a0a2f6d03c0fef939261b01d4c65
seaiam <65039814+seaiam@users.noreply.github.com>
2021-03-15 20:31:58 -0400

    #3
    Implemented comment option
    when clicking on a post , we get redirected to a page to comment on a certain post.

--- backend/pigeonpost/admin.py
+++ backend/pigeonpost/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Comments
 
 admin.site.register(Post)
-
+admin.site.register(Comments)
 
